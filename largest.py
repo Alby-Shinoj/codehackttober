b8fe44e8e897ed4b709faf4b0bea31ff92d543c6
srinjoy29 <96923080+srinjoy29@users.noreply.github.com>
2022-10-23 20:18:21 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,15 @@
+a= int(input("Enter the value of a"))
+b= int(input("Enter the value of b"))
+c= int(input("Enter the value of c"))
+
+if a>b:
+    if a>c:
+       print("a is largest",a)
+    else:
+       print("c is largest",c)
+else: 
+    if b>c:
+       print("b is largest",b)
+    else:
+       print("c is largest",c)
+ 
---
+++ suitable day.py
@@ -0,0 +1,20 @@
+a= int(input("Enter the suitable day number in between 1 to 7 :"))
+
+
+if a==1:
+  print("sunday")
+elif a==2:
+  print("Monday")
+elif a==3:
+  print("Tuesday")
+elif a==4:
+  print("Wednesday")
+elif a==5:
+  print("Thursday")
+elif a==6:
+  print("Friday")
+elif a==7:
+  print("Saturday")
+  
+else:
+    print("INPUT ERROR")
